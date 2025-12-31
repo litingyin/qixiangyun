d8938d6ad43043921a4e326055eb4d80ca54b4c9
Admin <14492689@qq.com>
2025-12-31 11:38:25 +0800

    为每个客户端创建单独的get方法，方便操作。

--- src/SDK.php
+++ src/SDK.php
@@ -4,6 +4,34 @@ namespace QixiangyunSDK;
 
 use QixiangyunSDK\Core\Config;
 use QixiangyunSDK\Core\ClientFactory;
+use QixiangyunSDK\Clients\InvoiceClient;
+use QixiangyunSDK\Clients\OrgClient;
+use QixiangyunSDK\Clients\AccountClient;
+use QixiangyunSDK\Clients\LoginClient;
+use QixiangyunSDK\Clients\TaxClient;
+use QixiangyunSDK\Clients\IitClient;
+use QixiangyunSDK\Clients\BsryglClient;
+use QixiangyunSDK\Clients\ProductClient;
+use QixiangyunSDK\Clients\OrgInfoClient;
+use QixiangyunSDK\Clients\MessageClient;
+use QixiangyunSDK\Clients\PhoneClient;
+use QixiangyunSDK\Clients\QdfpClient;
+use QixiangyunSDK\Clients\SpxxClient;
+use QixiangyunSDK\Clients\KhxxClient;
+use QixiangyunSDK\Clients\RzClient;
+use QixiangyunSDK\Clients\SdFileClient;
+use QixiangyunSDK\Clients\FpruzClient;
+use QixiangyunSDK\Clients\GjClient;
+use QixiangyunSDK\Clients\OrgTaxInfoClient;
+use QixiangyunSDK\Clients\QysClient;
+use QixiangyunSDK\Clients\ScjyClient;
+use QixiangyunSDK\Clients\ShbxClient;
+use QixiangyunSDK\Clients\CustomsClient;
+use QixiangyunSDK\Clients\CktsClient;
+use QixiangyunSDK\Clients\CollectClient;
+use QixiangyunSDK\Clients\RiskClient;
+use QixiangyunSDK\Clients\InsightClient;
+use QixiangyunSDK\Clients\LegislationClient;
 
 class SDK
 {
@@ -81,5 +109,285 @@ class SDK
     public static function create(array $config): self
     {
         return new self($config);
+    }
+
+    /**
+     * 获取发票客户端
+     *
+     * @return InvoiceClient
+     */
+    public function getInvoice(): InvoiceClient
+    {
+        return $this->getClient('invoice');
+    }
+
+    /**
+     * 获取企业管理客户端
+     *
+     * @return OrgClient
+     */
+    public function getOrg(): OrgClient
+    {
+        return $this->getClient('org');
+    }
+
+    /**
+     * 获取账号管理客户端
+     *
+     * @return AccountClient
+     */
+    public function getAccount(): AccountClient
+    {
+        return $this->getClient('account');
+    }
+
+    /**
+     * 获取登录客户端
+     *
+     * @return LoginClient
+     */
+    public function getLogin(): LoginClient
+    {
+        return $this->getClient('login');
+    }
+
+    /**
+     * 获取税务申报客户端
+     *
+     * @return TaxClient
+     */
+    public function getTax(): TaxClient
+    {
+        return $this->getClient('tax');
+    }
+
+    /**
+     * 获取个税客户端
+     *
+     * @return IitClient
+     */
+    public function getIit(): IitClient
+    {
+        return $this->getClient('iit');
+    }
+
+    /**
+     * 获取办税人员管理客户端
+     *
+     * @return BsryglClient
+     */
+    public function getBsrygl(): BsryglClient
+    {
+        return $this->getClient('bsrygl');
+    }
+
+    /**
+     * 获取产品管理客户端
+     *
+     * @return ProductClient
+     */
+    public function getProduct(): ProductClient
+    {
+        return $this->getClient('product');
+    }
+
+    /**
+     * 获取企业信息采集客户端
+     *
+     * @return OrgInfoClient
+     */
+    public function getOrgInfo(): OrgInfoClient
+    {
+        return $this->getClient('orginfo');
+    }
+
+    /**
+     * 获取消息客户端
+     *
+     * @return MessageClient
+     */
+    public function getMessage(): MessageClient
+    {
+        return $this->getClient('message');
+    }
+
+    /**
+     * 获取办税小号客户端
+     *
+     * @return PhoneClient
+     */
+    public function getPhone(): PhoneClient
+    {
+        return $this->getClient('phone');
+    }
+
+    /**
+     * 获取前台发票客户端
+     *
+     * @return QdfpClient
+     */
+    public function getQdfp(): QdfpClient
+    {
+        return $this->getClient('qdfp');
+    }
+
+    /**
+     * 获取商品信息客户端
+     *
+     * @return SpxxClient
+     */
+    public function getSpxx(): SpxxClient
+    {
+        return $this->getClient('spxx');
+    }
+
+    /**
+     * 获取客户信息客户端
+     *
+     * @return KhxxClient
+     */
+    public function getKhxx(): KhxxClient
+    {
+        return $this->getClient('khxx');
+    }
+
+    /**
+     * 获取认证客户端
+     *
+     * @return RzClient
+     */
+    public function getRz(): RzClient
+    {
+        return $this->getClient('rz');
+    }
+
+    /**
+     * 获取文件版式下载客户端
+     *
+     * @return SdFileClient
+     */
+    public function getSdFile(): SdFileClient
+    {
+        return $this->getClient('sdfile');
+    }
+
+    /**
+     * 获取发票入账客户端
+     *
+     * @return FpruzClient
+     */
+    public function getFpruz(): FpruzClient
+    {
+        return $this->getClient('fpruz');
+    }
+
+    /**
+     * 获取发票归集客户端
+     *
+     * @return GjClient
+     */
+    public function getGj(): GjClient
+    {
+        return $this->getClient('gj');
+    }
+
+    /**
+     * 获取企业税务信息客户端
+     *
+     * @return OrgTaxInfoClient
+     */
+    public function getOrgTaxInfo(): OrgTaxInfoClient
+    {
+        return $this->getClient('orgtaxinfo');
+    }
+
+    /**
+     * 获取企业税种客户端
+     *
+     * @return QysClient
+     */
+    public function getQys(): QysClient
+    {
+        return $this->getClient('qys');
+    }
+
+    /**
+     * 获取生产经营所得客户端
+     *
+     * @return ScjyClient
+     */
+    public function getScjy(): ScjyClient
+    {
+        return $this->getClient('scjy');
+    }
+
+    /**
+     * 获取社保客户端
+     *
+     * @return ShbxClient
+     */
+    public function getShbx(): ShbxClient
+    {
+        return $this->getClient('shbx');
+    }
+
+    /**
+     * 获取海关客户端
+     *
+     * @return CustomsClient
+     */
+    public function getCustoms(): CustomsClient
+    {
+        return $this->getClient('customs');
+    }
+
+    /**
+     * 获取出口退税客户端
+     *
+     * @return CktsClient
+     */
+    public function getCkts(): CktsClient
+    {
+        return $this->getClient('ckts');
+    }
+
+    /**
+     * 获取企业采集客户端
+     *
+     * @return CollectClient
+     */
+    public function getCollect(): CollectClient
+    {
+        return $this->getClient('collect');
+    }
+
+    /**
+     * 获取风险查询客户端
+     *
+     * @return RiskClient
+     */
+    public function getRisk(): RiskClient
+    {
+        return $this->getClient('risk');
+    }
+
+    /**
+     * 获取企业洞察客户端
+     *
+     * @return InsightClient
+     */
+    public function getInsight(): InsightClient
+    {
+        return $this->getClient('insight');
+    }
+
+    /**
+     * 获取政策法规客户端
+     *
+     * @return LegislationClient
+     */
+    public function getLegislation(): LegislationClient
+    {
+        return $this->getClient('legislation');
     }
 }
