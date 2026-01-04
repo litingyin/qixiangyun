808d9a845d116b8432df4d7a56ff5234f09c9027
Admin <14492689@qq.com>
2026-01-04 10:59:18 +0800

    新增地区代码和产品代码

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -45,6 +45,128 @@ class Config
      * @var string
      */
     protected $cacheDir;
+
+    /**
+     * 定义地区编码常量
+     */
+    // 直辖市
+    const AREA_CODE_BEIJING = '11';
+    const AREA_CODE_TIANJIN = '12';
+    const AREA_CODE_SHANGHAI = '31';
+    const AREA_CODE_CHONGQING = '50';
+    
+    // 省份
+    const AREA_CODE_HEBEI = '13';
+    const AREA_CODE_SHANXI = '14';
+    const AREA_CODE_NEIMENGGU = '15';
+    const AREA_CODE_LIAONING = '21';
+    const AREA_CODE_JILIN = '22';
+    const AREA_CODE_HEILONGJIANG = '23';
+    const AREA_CODE_JIANGSU = '32';
+    const AREA_CODE_ZHEJIANG = '33';
+    const AREA_CODE_ANHUI = '34';
+    const AREA_CODE_FUJIAN = '35';
+    const AREA_CODE_JIANGXI = '36';
+    const AREA_CODE_SHANDONG = '37';
+    const AREA_CODE_HENAN = '41';
+    const AREA_CODE_HUBEI = '42';
+    const AREA_CODE_HUNAN = '43';
+    const AREA_CODE_GUANGDONG = '44';
+    const AREA_CODE_GUANGXI = '45';
+    const AREA_CODE_HAINAN = '46';
+    const AREA_CODE_SICHUAN = '51';
+    const AREA_CODE_GUIZHOU = '52';
+    const AREA_CODE_YUNNAN = '53';
+    const AREA_CODE_XIZANG = '54';
+    const AREA_CODE_SHAANXI = '61';
+    const AREA_CODE_GANSU = '62';
+    const AREA_CODE_QINGHAI = '63';
+    const AREA_CODE_NINGXIA = '64';
+    const AREA_CODE_XINJIANG = '65';
+    
+    // 副省级城市
+    const AREA_CODE_DALIAN = '2102';      // 辽宁省大连市
+    const AREA_CODE_NINGBO = '3302';      // 浙江省宁波市
+    const AREA_CODE_XIAMEN = '3502';      // 福建省厦门市
+    const AREA_CODE_QINGDAO = '3702';     // 山东省青岛市
+    const AREA_CODE_SHENZHEN = '4403';    // 广东省深圳市
+    
+
+    /**
+     * 地区代码
+     * @var array
+     */
+    protected $areaCode = [
+        self::AREA_CODE_BEIJING => '北京市',
+        self::AREA_CODE_TIANJIN => '天津市',
+        self::AREA_CODE_HEBEI => '河北省',
+        self::AREA_CODE_SHANXI => '山西省',
+        self::AREA_CODE_NEIMENGGU => '内蒙古自治区',
+        self::AREA_CODE_LIAONING => '辽宁省',
+        self::AREA_CODE_DALIAN => '大连市',
+        self::AREA_CODE_JILIN => '吉林省',
+        self::AREA_CODE_HEILONGJIANG => '黑龙江省',
+        self::AREA_CODE_SHANGHAI => '上海市',
+        self::AREA_CODE_JIANGSU => '江苏省',
+        self::AREA_CODE_ZHEJIANG => '浙江省',
+        self::AREA_CODE_NINGBO => '宁波市',
+        self::AREA_CODE_ANHUI => '安徽省',
+        self::AREA_CODE_FUJIAN => '福建省',
+        self::AREA_CODE_XIAMEN => '厦门市',
+        self::AREA_CODE_JIANGXI => '江西省',
+        self::AREA_CODE_SHANDONG => '山东省',
+        self::AREA_CODE_QINGDAO => '青岛市',
+        self::AREA_CODE_HENAN => '河南省',
+        self::AREA_CODE_HUBEI => '湖北省',
+        self::AREA_CODE_HUNAN => '湖南省',
+        self::AREA_CODE_GUANGDONG => '广东省',
+        self::AREA_CODE_SHENZHEN => '深圳市',
+        self::AREA_CODE_GUANGXI => '广西',
+        self::AREA_CODE_HAINAN => '海南省',
+        self::AREA_CODE_CHONGQING => '重庆市',
+        self::AREA_CODE_SICHUAN => '四川省',
+        self::AREA_CODE_GUIZHOU => '贵州省',
+        self::AREA_CODE_YUNNAN => '云南省',
+        self::AREA_CODE_XIZANG => '西藏',
+        self::AREA_CODE_SHAANXI => '陕西省',
+        self::AREA_CODE_GANSU => '甘肃省',
+        self::AREA_CODE_QINGHAI => '青海省',
+        self::AREA_CODE_NINGXIA => '宁夏',
+        self::AREA_CODE_XINJIANG => '新疆',
+    ];
+
+    /**
+     * 产品编码常量
+     */
+    const PRODUCT_CODE_INVOICE_CHECK = '0001';
+    const PRODUCT_CODE_INVOICE_CHECK_EXTEND = '0007';
+    const PRODUCT_CODE_INVOICE_RECOGNITION = '0041';
+    const PRODUCT_CODE_INVOICE_ISSUE = '0004';
+    const PRODUCT_CODE_INVOICE_AUTH = '0003';
+    const PRODUCT_CODE_INVOICE_AUTH_GROUP = '0015';
+    const PRODUCT_CODE_INVOICE_ENTRY = '0070';
+    const PRODUCT_CODE_INVOICE_COLLECT = '0017';
+    const PRODUCT_CODE_INVOICE_COLLECT_GROUP = '0009';
+    const PRODUCT_CODE_INVOICE_DOWNLOAD = '0018';
+    const PRODUCT_CODE_INVOICE_DOWNLOAD_ENTERPRISE = '0005';
+
+    /**
+     * 产品编码
+     * @var array
+     */
+    protected $productCodes = [
+        self::PRODUCT_CODE_INVOICE_CHECK => '发票查验(标准版)',
+        self::PRODUCT_CODE_INVOICE_CHECK_EXTEND => '发票查验(扩展版)',
+        self::PRODUCT_CODE_INVOICE_RECOGNITION => '票据识别',
+        self::PRODUCT_CODE_INVOICE_ISSUE => '数电开票',
+        self::PRODUCT_CODE_INVOICE_AUTH => '发票认证(代账版)',
+        self::PRODUCT_CODE_INVOICE_AUTH_GROUP => '发票认证(集团版)',
+        self::PRODUCT_CODE_INVOICE_ENTRY => '发票入账',
+        self::PRODUCT_CODE_INVOICE_COLLECT => '发票归集(代账版)',
+        self::PRODUCT_CODE_INVOICE_COLLECT_GROUP => '发票归集(集团版)',
+        self::PRODUCT_CODE_INVOICE_DOWNLOAD => '数电票版式文件下载(代账版)',
+        self::PRODUCT_CODE_INVOICE_DOWNLOAD_ENTERPRISE => '数电票版式文件下载(企业版)',
+    ];
     
     /**
      * 默认配置
@@ -172,6 +294,48 @@ class Config
         return rtrim($this->apiHost, '/') . '/' . ltrim($endpoint, '/');
     }
     
+    /**
+     * 获取地区名称
+     *
+     * @param string $areaCode 地区编码
+     * @return string 地区名称，如果找不到则返回原编码
+     */
+    public function getAreaName(string $areaCode): string
+    {
+        return $this->areaCode[$areaCode] ?? $areaCode;
+    }
+
+    /**
+     * 获取所有地区编码和名称
+     *
+     * @return array 地区编码 => 地区名称的数组
+     */
+    public function getAllAreas(): array
+    {
+        return $this->areaCode;
+    }
+
+    /**
+     * 获取产品名称
+     *
+     * @param string $productCode 产品编码
+     * @return string 产品名称，如果找不到则返回原编码
+     */
+    public function getProductName(string $productCode): string
+    {
+        return $this->productCodes[$productCode] ?? $productCode;
+    }
+
+    /**
+     * 获取所有产品编码和名称
+     *
+     * @return array 产品编码 => 产品名称的数组
+     */
+    public function getAllProducts(): array
+    {
+        return $this->productCodes;
+    }
+
     /**
      * 更新配置
      *
