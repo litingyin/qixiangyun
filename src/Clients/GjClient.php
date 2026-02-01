495a7f39810740048e38242575df0da625955c96
Admin <14492689@qq.com>
2026-02-01 12:02:22 +0800

    修改库文件

--- src/Clients/GjClient.php
+++ src/Clients/GjClient.php
@@ -13,7 +13,7 @@ use QixiangyunSDK\Exceptions\QixiangyunException;
 class GjClient extends BaseClient
 {
     protected $clientName = 'gj';
-    
+
     /**
      * 获取客户端名称
      *
@@ -23,10 +23,10 @@ class GjClient extends BaseClient
     {
         return $this->clientName;
     }
-    
+
     /**
      * 申请全量发票汇总信息
-     * 
+     *
      * @param array $params 申请参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -34,13 +34,13 @@ class GjClient extends BaseClient
     public function applyInvoiceSummary(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/queryFpsljeseHj', $params);
     }
-    
+
     /**
      * 获取全量发票汇总信息
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -48,27 +48,25 @@ class GjClient extends BaseClient
     public function getInvoiceSummary(array $params): GjResponse
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/getFpsljeseHj', $params);
     }
-    
+
     /**
      * 发起进销项归集任务
-     * 
+     *
      * @param array $params 任务参数
      * @return GjResponse
      * @throws QixiangyunException
      */
     public function collectTask(array $params): GjResponse
     {
-        $this->validateParams($params, ['aggOrgId', 'type']);
-        
         return $this->requestGjResponse('/v2/invoice/gj/collectTask', $params);
     }
-    
+
     /**
      * 查询归集任务结果
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -76,13 +74,13 @@ class GjClient extends BaseClient
     public function queryCollectTaskStatus(array $params): GjResponse
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/collectTaskStatus', $params);
     }
-    
+
     /**
      * 获取发票数据
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -90,13 +88,13 @@ class GjClient extends BaseClient
     public function getCollectTaskPageData(array $params): GjResponse
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/getCollectTaskPageData', $params);
     }
-    
+
     /**
      * 发起作废/红冲归集任务
-     * 
+     *
      * @param array $params 任务参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -104,13 +102,13 @@ class GjClient extends BaseClient
     public function redInvoiceCollectTask(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/redInvoiceCollectTask', $params);
     }
-    
+
     /**
      * 发起已勾选归集任务
-     * 
+     *
      * @param array $params 任务参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -118,13 +116,13 @@ class GjClient extends BaseClient
     public function collectCheckedInvoice(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/collectCheckedInvoice', $params);
     }
-    
+
     /**
      * 查询已勾选归集任务结果
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -132,13 +130,13 @@ class GjClient extends BaseClient
     public function collectCheckedInvoiceStatus(array $params): GjResponse
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/collectCheckedInvoiceStatus', $params);
     }
-    
+
     /**
      * 查询已勾选归集数据
-     * 
+     *
      * @param array $params 查询参数
      * @return array
      * @throws QixiangyunException
@@ -146,13 +144,13 @@ class GjClient extends BaseClient
     public function getCollectCheckedInvoicePageData(array $params)
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->request('/v2/invoice/gj/getCollectCheckedInvoicePageData', $params);
     }
-    
+
     /**
      * 撤销归集待执行任务
-     * 
+     *
      * @param array $params 撤销参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -160,13 +158,13 @@ class GjClient extends BaseClient
     public function terminateCollectionTask(array $params): GjResponse
     {
         $this->validateParams($params, ['taskId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/terminateCollectionTask', $params);
     }
-    
+
     /**
      * 进销项发票信息
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -174,13 +172,13 @@ class GjClient extends BaseClient
     public function syncCollectTask(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/syncCollectTask', $params);
     }
-    
+
     /**
      * 发票基础信息
-     * 
+     *
      * @param array $params 查询参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -188,13 +186,13 @@ class GjClient extends BaseClient
     public function syncFpjbxx(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/syncFpjbxx', $params);
     }
-    
+
     /**
      * 发票详情归集
-     * 
+     *
      * @param array $params 归集参数
      * @return GjResponse
      * @throws QixiangyunException
@@ -202,7 +200,7 @@ class GjClient extends BaseClient
     public function collectFpmx(array $params): GjResponse
     {
         $this->validateParams($params, ['aggOrgId', 'fpdm', 'fphm']);
-        
+
         return $this->requestGjResponse('/v2/invoice/gj/fpmx', $params);
     }
 }
--- src/Clients/PhoneClient.php
+++ src/Clients/PhoneClient.php
@@ -13,7 +13,7 @@ use QixiangyunSDK\Exceptions\QixiangyunException;
 class PhoneClient extends BaseClient
 {
     protected $clientName = 'phone';
-    
+
     /**
      * 获取客户端名称
      *
@@ -23,10 +23,10 @@ class PhoneClient extends BaseClient
     {
         return $this->clientName;
     }
-    
+
     /**
      * 申请小号订单
-     * 
+     *
      * @param array $params 订单参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -34,13 +34,13 @@ class PhoneClient extends BaseClient
     public function createOrder(array $params): PhoneResponse
     {
         $this->validateParams($params, ['aggOrgId', 'accountCount']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/createBsxhOrder', $params);
     }
-    
+
     /**
      * 查询小号订单详情
-     * 
+     *
      * @param array $params 查询参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -48,13 +48,13 @@ class PhoneClient extends BaseClient
     public function queryOrder(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/queryBsxhOrder', $params);
     }
-    
+
     /**
      * 配置小号订单开通通知地址
-     * 
+     *
      * @param array $params 配置参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -62,13 +62,13 @@ class PhoneClient extends BaseClient
     public function editPushUrl(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId', 'pushUrl']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/editBsxhOrderPushUrlConfig', $params);
     }
-    
+
     /**
      * 绑定实名手机号与办税小号
-     * 
+     *
      * @param array $params 绑定参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -76,13 +76,13 @@ class PhoneClient extends BaseClient
     public function bind(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId', 'phoneNumber', 'taxNo']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/bindAxn', $params);
     }
-    
+
     /**
      * 更换绑定的实名手机号
-     * 
+     *
      * @param array $params 更新参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -90,13 +90,13 @@ class PhoneClient extends BaseClient
     public function updatePhone(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId', 'newPhoneNumber', 'taxNo']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/updateNoA', $params);
     }
-    
+
     /**
      * 解除实名手机号与办税小号绑定
-     * 
+     *
      * @param array $params 解绑参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -104,13 +104,13 @@ class PhoneClient extends BaseClient
     public function unbind(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId', 'taxNo']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/unbind', $params);
     }
-    
+
     /**
      * 办税小号通话清单查询
-     * 
+     *
      * @param array $params 查询参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -118,27 +118,25 @@ class PhoneClient extends BaseClient
     public function queryCallList(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/querySecretReportPageList', $params);
     }
-    
+
     /**
      * 查询短信验证码
-     * 
+     *
      * @param array $params 查询参数
      * @return PhoneResponse
      * @throws QixiangyunException
      */
     public function querySmsCode(array $params): PhoneResponse
     {
-        $this->validateParams($params, ['orderId']);
-        
         return $this->requestPhoneResponse('/v2/public/sjhm/queryNote', $params);
     }
-    
+
     /**
      * 配置小号短信通知地址
-     * 
+     *
      * @param array $params 配置参数
      * @return PhoneResponse
      * @throws QixiangyunException
@@ -146,7 +144,7 @@ class PhoneClient extends BaseClient
     public function editSmsPushUrl(array $params): PhoneResponse
     {
         $this->validateParams($params, ['orderId', 'pushUrl']);
-        
+
         return $this->requestPhoneResponse('/v2/public/sjhm/editPushSmsConfig', $params);
     }
 }
